52072631ea9ef62cea635d9f17412f5d553693df
qstommyshu <qstommyshu@gmail.com>
2021-10-02 16:41:32 -0400

    getlocation

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  let getLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log("location type is: " + typeof(location))
+    text = JSON.stringify(location.coords);
+    console.log(text)
+    console.log("text type is :" + typeof(text))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working the app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
+      <Text>{typeof(text)}</Text>
+      <Text>1</Text>
     </View>
   );
 }
@@ -14,8 +43,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
   },
 });
